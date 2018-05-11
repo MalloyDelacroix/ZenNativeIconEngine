eee1b2c897a8538fa20c987880251160c0ae46b4
Kyle <kyle.hickey222@gmail.com>
2018-05-11 15:27:37 -0500

    Add first test case

--- src/test/java/core/IconEngineTest.java
+++ src/test/java/core/IconEngineTest.java
@@ -1,15 +1,16 @@
 package core;
 
-import NativeAccess.WindowsIconExtractor;
-import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
 
-import javax.imageio.ImageIO;
+import javafx.scene.image.Image;
+import org.junit.jupiter.api.Test;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class IconEngineTest {
@@ -18,10 +19,9 @@ public class IconEngineTest {
     void getIconImage() throws URISyntaxException, IOException {
         URL resource = IconEngineTest.class.getClassLoader().getResource("test_icon.png");
         File testFile = new File(resource.toURI());
-        WindowsIconExtractor mockExtractor = mock(WindowsIconExtractor.class);
-        when(mockExtractor.getIcon(testFile.getPath())).thenReturn(ImageIO.read(testFile));
 
-
+        assertTrue(IconEngine.getIconBufferedImage(testFile) instanceof BufferedImage);
+        assertTrue(IconEngine.getIconImage(testFile) instanceof Image);
     }
 
 }
